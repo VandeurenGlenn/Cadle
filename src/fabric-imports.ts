fd0e9d3d0ae61027fa892e3bbce85936479720ab
vandeurenglenn <vandeurenglenn@gmail.com>
2023-05-27 19:41:01 +0200

    Create fabric-imports.ts

---
+++ src/fabric-imports.ts
@@ -0,0 +1,6 @@
+import * as fabric from 'fabric'
+globalThis.fabric = fabric
+
+const { Canvas, Circle, Line, IText, Object, loadSVGFromURL, util } = fabric
+
+export { Canvas, Circle, Line, IText, Object, loadSVGFromURL, util }

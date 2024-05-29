558aec564018556961187c65e2d884f5cafcacb0
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-05-29 09:45:15 +0200

    fix(keyboard): copy paste

--- src/controllers/keyboard/commands/copy.ts
+++ src/controllers/keyboard/commands/copy.ts
@@ -2,14 +2,15 @@ import { ActiveSelection, Group } from 'fabric'
 import { clipboard, getActiveObject } from '../../../utils.js'
 import { isMac } from '../utils.js'
 
-export const isCopy = ({metaKey, key, ctrlKey}: KeyboardEvent) =>
-  key === 'c' && (isMac ? metaKey : ctrlKey)
+export const isCopy = ({ metaKey, key, ctrlKey }: KeyboardEvent) => key === 'c' && (isMac ? metaKey : ctrlKey)
 
 export const copy = async () => {
-  const cloned = await getActiveObject()?.clone() as ActiveSelection
-    
-  clipboard.object = cloned.type === 'group' ? cloned : new Group(cloned._objects, {
-    subTargetCheck: true,
-    interactive: true
-  })
+  const cloned = (await getActiveObject()?.clone()) as ActiveSelection
+
+  clipboard.object = cloned
+
+  // clipboard.object = cloned.type === 'group' ? cloned : new Group(cloned._objects, {
+  //   subTargetCheck: true,
+  //   interactive: true
+  // })
 }
--- src/controllers/keyboard/commands/paste.ts
+++ src/controllers/keyboard/commands/paste.ts
@@ -1,34 +1,33 @@
 import { clipboard, canvas, positionObject } from '../../../utils.js'
 import { isMac } from '../utils.js'
 
-export const isPaste = ({metaKey, key, ctrlKey}: KeyboardEvent) => key === 'v' && (isMac ? metaKey : ctrlKey)
+export const isPaste = ({ metaKey, key, ctrlKey }: KeyboardEvent) => key === 'v' && (isMac ? metaKey : ctrlKey)
 
 export const paste = async () => {
-  await canvas.discardActiveObject();
+  await canvas.discardActiveObject()
   const { left, top } = positionObject()
   const cloned = await clipboard.object?.clone()
-  
+
   if (cloned) {
     cloned.set({
-      left: left - (cloned.width / 2),
-      top: top - (cloned.height / 2),
+      left: left - cloned.width / 2,
+      top: top - cloned.height / 2,
       evented: true
     })
-    
+
     if (cloned.type === 'activeSelection') {
       // active selection needs a reference to the canvas.
-      cloned.canvas = canvas;
-      cloned.forEachObject(function(obj) {
-        canvas.add(obj);
-      });
+      cloned.canvas = canvas
+      cloned.forEachObject(function (obj) {
+        canvas.add(obj)
+      })
       // this should solve the unselectability
-      cloned.setCoords();
+      cloned.setCoords()
     } else {
-      canvas.add(cloned);
+      canvas.add(cloned)
     }
-    
-    await canvas.setActiveObject(cloned);
+
+    canvas.shouldRender = true
+    await canvas.setActiveObject(cloned)
   }
-  canvas.shouldRender = true
-  
 }

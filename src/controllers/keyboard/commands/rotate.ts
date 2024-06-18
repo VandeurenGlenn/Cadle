67af2a7d0ce0d440c111f4846c42260a44d5fbb6
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-06-18 18:30:29 +0200

    Update rotate.ts

--- src/controllers/keyboard/commands/rotate.ts
+++ src/controllers/keyboard/commands/rotate.ts
@@ -3,7 +3,7 @@ import { isMac } from '../utils.js'
 
 // only flip on active object
 export const isRotate = ({ metaKey, key, ctrlKey, shiftKey }: KeyboardEvent) =>
-  canvas.getActiveObject() && (key === '+' || key === '-') && (isMac ? metaKey : ctrlKey)
+  canvas.getActiveObject() && (key === '+' || key === '-')
 
 export const rotate = ({ key }) => {
   canvas.shouldRender = true

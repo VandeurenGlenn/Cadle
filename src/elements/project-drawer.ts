4172d7bf0aafcb1951fb96271c8cdbc5e22c9bf3
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-06-18 18:24:30 +0200

    Update project-drawer.ts

--- src/elements/project-drawer.ts
+++ src/elements/project-drawer.ts
@@ -1,18 +1,19 @@
-import { LitElement, html, css } from 'lit';
-import { map } from 'lit/directives/map.js';
+import { LitElement, html, css } from 'lit'
+import { map } from 'lit/directives/map.js'
 import { customElement, property } from 'lit/decorators.js'
 import '@vandeurenglenn/lit-elements/button.js'
 import '@vandeurenglenn/lit-elements/selector.js'
 import './catalog/catalog.js'
 import './project/project.js'
 
-
 @customElement('project-drawer')
 export class ProjectDrawer extends LitElement {
-  
-
   get #pages() {
     return this.renderRoot.querySelector('custom-pages')
+  }
+
+  select(selected) {
+    this.#pages.select(selected)
   }
 
   set action(value) {
@@ -31,7 +32,9 @@ export class ProjectDrawer extends LitElement {
         width: 100%;
         --md-list-item-list-item-container-color: #fff;
         --md-list-item-list-item-leading-avatar-color: #fff;
-        --md-list-item-list-item-leading-avatar-shape: 0
+        --md-list-item-list-item-leading-avatar-shape: 0;
+        pointer-events: auto;
+        border-top: 1px solid var(--md-sys-color-outline);
       }
 
       section {
@@ -43,34 +46,21 @@ export class ProjectDrawer extends LitElement {
       custom-button {
         pointer-events: auto;
       }
-
-      custom-selector {
-        width: 100%;
-        height: auto;
-        flex-direction: row;
-        pointer-events: auto;
-      }
     `
-  ];
+  ]
 
   _dragstart(e) {
-    console.log(e);
-    
+    console.log(e)
   }
 
   render() {
     return html`
-        <custom-selector attr-for-selected="route" @selected=${(e) => this.#pages.select(e.detail)}>
-          <custom-button route="project" label="project" type="outlined"></custom-button>
-          <custom-button route="symbols" label="catalog"></custom-button>
-        </custom-selector>
-
-        <custom-pages attr-for-selected="data-route">
-          <project-element data-route="project"></project-element>
-          <catalog-element data-route="symbols"></catalog-element>
-        </custom-pages>
-
-        <slot></slot>
-    `;
+      <custom-pages
+        attr-for-selected="data-route"
+        default-selected="symbols">
+        <project-element data-route="project"></project-element>
+        <catalog-element data-route="symbols"></catalog-element>
+      </custom-pages>
+    `
   }
 }

dcc9dcfc5268187ec4faab1ed81a5dea069abd9d
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-06-18 18:32:27 +0200

    Add category to symbol

--- generate-manifest.js
+++ generate-manifest.js
@@ -6,15 +6,14 @@ const manifest = []
 
 for (const category of categories) {
   if (category !== '.DS_Store' && category !== 'manifest.js') {
-    let symbols = (await readdir(join('./www/symbols', category)))
-      .map(symbol => ({
-        name: symbol.replace('.svg', ''),
-        path: join('./symbols', category, symbol)
-      }))
-      
+    let symbols = (await readdir(join('./www/symbols', category))).map((symbol) => ({
+      type: category,
+      name: symbol.replace('.svg', '').toLowerCase(),
+      path: join('./symbols', category, symbol)
+    }))
+
     manifest.push({ category, symbols })
   }
-  
 }
 
 writeFile('./www/symbols/manifest.js', `export default ${JSON.stringify(manifest, null, '\t')}`)

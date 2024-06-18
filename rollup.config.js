5f9ac682ae06da49c1eb0278eccd6c7b9cd0d826
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-06-18 18:31:58 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import { join } from 'path'
 import { cp } from 'fs/promises'
 import materialSymbols from 'rollup-plugin-material-symbols'
 
-await cp('node_modules/@vandeurenglenn/custom-elements/exports/themes/default', 'www/themes/default', {
+await cp('node_modules/@vandeurenglenn/lit-elements/exports/themes/default', 'www/themes/default', {
   recursive: true
 })
 

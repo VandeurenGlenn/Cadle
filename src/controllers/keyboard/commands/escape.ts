8a92447da1deb1e032b37eb5e2d5e744aa9bb200
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-15 19:34:42 +0200

    Fix escape when freedrawing

--- src/controllers/keyboard/commands/escape.ts
+++ src/controllers/keyboard/commands/escape.ts
@@ -3,14 +3,17 @@ import { canvas, clipboard, getActiveObjects } from '../../../utils.js'
 export const isEscape = ({ key }: KeyboardEvent) => key === 'Escape'
 
 export const escape = () => {
-  
-  
   if (canvas.getActiveObjects()?.length) {
     canvas.discardActiveObject()
     canvas.shouldRender = true
     return
   }
-    
-    if (cadleShell.action) return cadleShell.action=undefined
-  
+
+  if (cadleShell.action === 'draw') {
+    cadleShell.action = undefined
+    cadleShell.field.canvas.isDrawingMode = false
+    cadleShell.field.canvas.renderAll()
+    return
+  }
+  if (cadleShell.action) return (cadleShell.action = undefined)
 }
